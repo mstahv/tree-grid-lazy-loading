bc4526b803b73d0dfe57c4bcb119c6509ceeaa96
Matti Tahvonen <matti@vaadin.com>
2023-11-01 20:33:31 +0200

    cleanup

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,19 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	/**
+	 * DO NOT USE THIS to run to the demo, unless you setup
+	 * the DB connection and the DB schema yourself. For playing
+	 * around with the demo, use the LocalDevApplication class
+	 * that uses TestContainers to launch a test DB with Docker.
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
