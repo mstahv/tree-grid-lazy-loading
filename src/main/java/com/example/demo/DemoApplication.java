8191d774612545c5fa9761669767f335d5b0d9e1
Matti Tahvonen <matti@vaadin.com>
2023-11-01 20:38:23 +0200

    cleanup

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,14 @@ import java.nio.charset.StandardCharsets;
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
+	/**
+	 * DO NOT USE THIS to run to the demo, unless you setup
+	 * the DB connection and the DB schema yourself. For playing
+	 * around with the demo, use the LocalDevApplication class
+	 * that uses TestContainers to launch a test DB with Docker.
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
